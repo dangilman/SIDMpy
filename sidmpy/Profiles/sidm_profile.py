5b634296b8fc09cedd550814865ac349d4604498
Daniel Gilman <gilmanda@ucla.edu>
2020-10-02 12:53:12 -0400

    update

--- sidmpy/Profiles/sidm_profile.py
+++ sidmpy/Profiles/sidm_profile.py
@@ -5,6 +5,7 @@ from sidmpy.Profiles.nfw import *
 from astropy.cosmology import FlatLambdaCDM
 from scipy.interpolate import interp1d
 from scipy.ndimage.filters import gaussian_filter
+from scipy.integrate import quad
 import numpy as np
 from sidmpy.CrossSections.power_law import PowerLaw
 
@@ -71,6 +72,11 @@ class SIDMprofile(object):
 
         self.rhos, self.rs = rhonfw, rs_nfw
 
+        self._rmax_mass = 10000*rs_nfw
+        r_values = np.linspace(self.rmin, self._rmax_mass, 500)
+        M = [self.enclosed_mass(ri) for ri in r_values]
+        self._mass_enclosed_interp = interp1d(r_values, M, fill_value=0)
+
     def sample_from_profile(self, Npoints, rmin, rmax):
 
         points = []
@@ -95,6 +101,30 @@ class SIDMprofile(object):
 
         return np.array(points)
 
+    def enclosed_mass(self, r):
+
+        if hasattr(self, '_mass_enclosed_interp'):
+            return self._mass_enclosed_interp(r)
+
+        M = quad(self._mass_enclosed_integrand, self.rmin, r)[0]
+        return M
+
+    def radial_velocity_dispersion(self, r):
+
+        rmax = self._rmax_mass
+        (rho, _) = self(r)
+        integral = quad(self._velocity_dispersion_integrand, r, rmax)[0]
+        return np.sqrt(integral / rho)
+
+    def _velocity_dispersion_integrand(self, r):
+
+        (rho, _) = self(r)
+        return rho * self.enclosed_mass(r) / r ** 2
+
+    def _mass_enclosed_integrand(self, r):
+        (rho, _) = self(r)
+        return 4 * np.pi * r ** 2 * rho
+
     def __call__(self, r, rt=100000, smooth=False, smooth_scale=0.01):
 
 
@@ -116,8 +146,8 @@ class SIDMprofile(object):
         else:
 
             if r <= self.rmin:
-                return 0.
+                return 0., 0
             elif r <= self._rmatch:
-                return self.rho_iso_interp(r)
+                return self.rho_iso_interp(r), TNFWprofile(r, self.rhos, self.rs, rt)
             else:
                 return TNFWprofile(r, self.rhos, self.rs, rt), TNFWprofile(r, self.rhos, self.rs, rt)
