2f15ee9d319c828c9d955d0a3253b4c0abe019b1
Daniel Gilman <gilmanda@ucla.edu>
2021-02-12 12:19:10 -0400

    add compute deflection angles

---
+++ sidmpy/Solver/solve_deflection_angles.py
@@ -0,0 +1,22 @@
+import numpy
+import sys
+from sidmpy.Profiles.deflection_angle_util import deflection
+from sidmpy.Profiles.nfw_like import coreTNFWprofile
+
+numpy.set_printoptions(threshold=sys.maxsize)
+x = numpy.logspace(-3, 2, 50)
+tau_values = numpy.arange(1, 31, 1)
+beta_values = numpy.arange(0.0025, 1.005, 0.005)
+
+def solve_deflection_angles(nproc=10):
+
+    log_deflection_array = numpy.empty((len(beta_values), len(tau_values), len(x)))
+
+    for i, beta in enumerate(beta_values):
+        for j, tau in enumerate(tau_values):
+            function_args = (x, 1., 1., tau, beta)
+            deflection_args = (x, coreTNFWprofile, function_args, True, nproc)
+            deflection_angle = deflection(*deflection_args)
+            log_deflection_array[i, j, :] = numpy.log10(deflection_angle)
+
+    return log_deflection_array
