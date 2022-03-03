be3139378449ce72bcbd02f81fcf0bb1bf3b171a
Daniel Gilman <gilmanda@ucla.edu>
2022-03-03 12:56:25 -0400

    update

--- sidmpy/CrossSections/resonant_constant.py
+++ sidmpy/CrossSections/resonant_constant.py
@@ -1,14 +1,17 @@
 from sidmpy.CrossSections.cross_section import InteractionCrossSection
 from sidmpy.CrossSections.velocity_independent import VelocityIndependentCrossSection
-
+import numpy as np
 
 class ResonantConstant(InteractionCrossSection):
 
     """
     This implements a velocity-dependent cross section of the form
 
-    sigma(v) = norm / (1 + (v/v_ref)^2 )^2
+    sigma(v) = s / (1 + x^2)
+    where x = (v - v_res) / w_res describes resonant feature
 
+    For numerical stability, the cross section amplitude far from the resonance is set to an extremely
+    small non-zero number
     """
 
     def __init__(self, norm, v_res, w_res, res_amplitude):
@@ -16,6 +19,7 @@ class ResonantConstant(InteractionCrossSection):
         self._vres = v_res
         self._res_amp = res_amplitude
         self._wres = w_res
+        self._norm = norm
         self._vindep = VelocityIndependentCrossSection(norm)
         super(ResonantConstant, self).__init__(norm, self._velocity_dependence_kernel)
 
@@ -34,3 +38,22 @@ class ResonantConstant(InteractionCrossSection):
         x = (v - self._vres)/self._wres
         kernel_resonance = self._res_amp / (1 + x**2)
         return kernel_resonance/self.norm + kernel_vindep
+
+class ExpResonantConstant(ResonantConstant):
+
+    """
+    This implements a velocity-dependent cross section of the form
+
+    sigma(v) = s * exp(-x^2/2)
+    where x = (v - v_res) / w_res describes resonant feature
+
+    For numerical stability, the cross section amplitude far from the resonance is set to an extremely
+    small non-zero number
+    """
+
+    def _velocity_dependence_kernel(self, v):
+
+        kernel_tchannel = self._vindep.evaluate(v)/self.norm
+        x = (v - self._vres)/self._wres
+        kernel_resonance = self._res_amp * np.exp(-0.5 * x**2)
+        return kernel_resonance/self.norm + kernel_tchannel
